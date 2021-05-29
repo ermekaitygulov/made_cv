5ccd06187a0378c84293872a4558f3899eeb8ebb
ermekaitygulov <ermek.imankulov@gmail.com>
2021-05-29 13:51:58 +0300

    add seg_size param to sumbit config

--- Lab02_NMT/create_submission.py
+++ Lab02_NMT/create_submission.py
@@ -39,7 +39,7 @@ def main(args):
     config = read_config(args.config)
 
     print("Start inference")
-    w, h = config['size']
+    w, h = config['rec_size']
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -55,7 +55,7 @@ def main(args):
 
         # 1. Segmentation.
         image, k, dw, dh = prepare_for_segmentation(image_src.astype(np.float) / 255.,
-                                                    (256, 256))
+                                                    (config['seg_size']))
         x = torch.from_numpy(image.transpose(2, 0, 1)).float().unsqueeze(0)
         with torch.no_grad():
             pred = torch.sigmoid(segmentation_model(x.to(device))).squeeze().cpu().numpy()
